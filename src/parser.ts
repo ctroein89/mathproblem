e39d7f93e7ae74e3c3b565106ee22590315704d4
Chris Troein <chris@troe.in>
2022-11-14 22:47:15 -0500

    Handle addition and multiplication

--- src/parser.ts
+++ src/parser.ts
@@ -1,29 +1,32 @@
 export function parseExpression(expression: string) {
-	let val1 = "";
+	let left = "";
 	let operator;
-	let val2 = "";
+	let right = "";
 	let hit1 = false;
 	let digitCheck = /[\d]/;
 
-	for(let i = expression.length; i>=0; i--) {
+	for(let i = 0; i<expression.length; i++) {
 		if (digitCheck.test(expression[i]) && !hit1) {
-			val1 = val1 + expression[i];
-			console.log("val1", val1)
+			left = left + expression[i];
+			console.log("left", left)
 		} else if (/[\*+]/.test(expression[i])) {
 			operator = expression[i];
 			hit1 = true;
 			console.log("operator", operator)
 		} else if (digitCheck.test(expression[i])) {
-			val2 = val2 + expression[i];
-			console.log("val2", val2)
+			right = right + expression[i];
+			console.log("right", right)
+		} else if (/ /.test(expression[i])) {
+		} else {
+			throw new Error("Unrecognized character: '" + expression[i] + "'")
 		}
 	}
 
 	if (operator == "*") {
-		return parseInt(val1) * parseInt(val2);
+		return parseInt(left) * parseInt(right) + "";
 	}
 	if (operator == "+") {
-		return parseInt(val1) + parseInt(val2);
+		return parseInt(left) + parseInt(right) + "";
 	}
 
 	return -1;
--- tests/test.ts
+++ tests/test.ts
@@ -3,11 +3,21 @@ import * as parser from '../src/parser'
 describe("silly function", () => {
   test("multiplication", () => {
     let expression = "7*8"
-    expect(parser.parseExpression(expression)).toBe(56)
+    expect(parser.parseExpression(expression)).toBe("56")
   })
 
   test("addition only", () => {
     let expression = "7 + 8"
-    expect(parser.parseExpression(expression)).toBe(15)
+    expect(parser.parseExpression(expression)).toBe("15")
+  })
+
+  xtest("add then multiple", () => {
+    let expression = "(2 + 7) * 8"
+    expect(parser.parseExpression(expression)).toBe("58")
+  })
+
+  xtest("add then multiple", () => {
+    let expression = "(2 * 7) + 8"
+    expect(parser.parseExpression(expression)).toBe("22")
   })
 })
